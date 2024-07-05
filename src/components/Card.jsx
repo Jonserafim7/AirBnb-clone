237de9092cd1f0aabdb4e638cd7c9d87f1c5f269
Jonas <142022025+Jonserafim7@users.noreply.github.com>
2024-07-05 00:03:55 -0300

    updated img paths again

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import StarIcon from '/public/assets/Star.png';
-import EllipseIcon from '/public/assets/Ellipse6.png';
+import StarIcon from "/assets/Star.png";
+import EllipseIcon from "/assets/Ellipse6.png";
 
+const Card = (props) => {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
 
-const Card = (props) => {  
-    let badgeText
-    if (props.openSpots === 0) {
-        badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
-        badgeText = "ONLINE"
-    }
-    
-    return (
-        <div className="card">
-            {badgeText && <div className='tag'>{badgeText}</div>}
-            <img className="card-image" src={`/public/assets/${props.coverImg}`} alt='Katie Zaferes'/>
-            <div className='card-text'>
-                <div className='rating'>
-                    <img className='star-icon' src={StarIcon} alt='start icon'/>
-                    <p className='rating'>{props.stats.rating}</p>
-                    <p className='number-of-ratings'>({props.stats.reviewCount})</p>
-                    <img className='ellipse-icon' src={EllipseIcon}/>
-                    <p className='location'>{props.location}</p>
-                </div>
-                <h2 className='description'>{props.title}</h2>
-                <p><span className='bold'>From ${props.price}</span> / person</p>
-            </div>
+  return (
+    <div className="card">
+      {badgeText && <div className="tag">{badgeText}</div>}
+      <img
+        className="card-image"
+        src={`/assets/${props.coverImg}`}
+        alt="Katie Zaferes"
+      />
+      <div className="card-text">
+        <div className="rating">
+          <img className="star-icon" src={StarIcon} alt="start icon" />
+          <p className="rating">{props.stats.rating}</p>
+          <p className="number-of-ratings">({props.stats.reviewCount})</p>
+          <img className="ellipse-icon" src={EllipseIcon} />
+          <p className="location">{props.location}</p>
         </div>
-    );
+        <h2 className="description">{props.title}</h2>
+        <p>
+          <span className="bold">From ${props.price}</span> / person
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
